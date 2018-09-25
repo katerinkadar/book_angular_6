2ffa6468225455ae9e6ab07cc84f078794a4b6ae
katerinkadar <katerinkadar@mail.ru>
2018-09-25 20:25:47 +0300

    Добавила Бестселлеры

--- BookAngular6/src/app/app.module.ts
+++ BookAngular6/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 import { BookDetailComponent } from './book-detail/book-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksComponent,
-    BookDetailComponent
+    BookDetailComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
